761e6a19ec8f9ac6a0bccac703d7a4f95a05f3db
Elwin <abc602456615@163.com>
2020-03-24 01:34:23 +0000

    Unchecked register complete

--- miniprogram/pages/exchange/register.js
+++ miniprogram/pages/exchange/register.js
@@ -5,27 +5,74 @@ Page({
    */
   data: {
     array: ["医用物品", "消毒用品", "生活用品", "其他用品"],
-    disabled: true,
+    disabled: false,
     defaultSize: 'default',
     plain: false,
     loading: false,
+    register_catagory: '',
+    register_name: '',
+    register_amount: 0
   },
 
+  // Only store the index value to storage for space complexity
   bindPickerChange: function(e) {
     this.setData({
-      index: e.detail.value
+      index: e.detail.value,
+      register_catagory: e.detail.value
+    })
+  },
+
+  name: function (e) {
+    this.setData({
+      register_name: e.detail.value
+    })
+  },
+
+  amount: function (e) {
+    this.setData({
+      register_amount: e.detail.value
     })
   },
 
   submission: function () {
     // submission function
+    const db = wx.cloud.database()
+    db.collection("Item").add({
+      data: {
+        type: this.data.register_catagory,
+        name: this.data.register_name,
+        amount: this.data.register_amount
+      },
+      success: function(res) {
+        wx.showToast({
+          title: 'Success!',
+          icon: 'success',
+          duration: 2000,
+          mask: false,
+          success: function() {
+            setTimeout(function() {
+              wx.navigateBack({
+                delta: 1
+              })
+            }, 2000)
+          }
+        })
+      },
+      fail: function(res) {
+        wx.showToast({
+          title: 'Failed...',
+          duration: 2000,
+          mask: true
+        })
+      },
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    // 从用户获取权限： OpenID等等
   },
 
   /**
