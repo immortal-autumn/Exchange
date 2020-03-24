e1d7801b3edce5ff6c2cd0968c9de85122066f7c
Elwin <abc602456615@163.com>
2020-03-24 19:11:42 +0000

    加了一点细节

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -68,21 +68,6 @@ Page({
     }
   },
 
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-      },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-      }
-    })
-  },
-
   // 上传图片
   doUpload: function () {
     // 选择图片
